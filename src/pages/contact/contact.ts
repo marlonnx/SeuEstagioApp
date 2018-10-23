8739e9cdc5c3207224d28ff555b8622437c32180
Marlon <felipemarlon.san@gmail.com>
2018-10-23 09:36:09 -0300

    commit

--- src/pages/contact/contact.ts
+++ src/pages/contact/contact.ts
@@ -1,14 +1,32 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, LoadingController } from 'ionic-angular';
 
 @Component({
   selector: 'page-contact',
   templateUrl: 'contact.html'
 })
 export class ContactPage {
-
-  constructor(public navCtrl: NavController) {
+show = true;
+  constructor(public navCtrl: NavController,public loadingCtrl: LoadingController) {
 
   }
+  presentLoading() {
+    const loader = this.loadingCtrl.create({
+      content: "Enviando...",
+      duration: 1500
+    });
+    loader.present();
+  }
+  func() {
+    this.presentLoading();
 
+    setTimeout(() => {
+      this.show = false;
+    }, 2100);
+    this.presentLoading();
+  }
+
+  ionViewWillEnter(){
+    this.show = true;
+  }
 }
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -7,7 +7,43 @@ import { JobDetailPage } from '../job-detail/job-detail';
   templateUrl: 'home.html'
 })
 export class HomePage {
-
+  hotJobs = [
+    {
+      jobTitle: 'Estagio Cursando Direito - Belo Horizonte',
+      jobDescription: ' Futura Estágios Jurídica - Trabalhista amet culpa praesentium necessitatibus facere quae dignissimos sunt.',
+      jobCreatedAt:'13/09 09:25 - Belo Horizonte, MG'
+    },
+    {
+      jobTitle: 'Estagio Cursando Direito - Belo Horizonte',
+      jobDescription: ' Futura Estágios Jurídica - Trabalhista amet culpa praesentium necessitatibus facere quae dignissimos sunt.',
+      jobCreatedAt:'13/09 09:25 - Belo Horizonte, MG'
+    },
+    {
+      jobTitle: 'Estagio Cursando Direito - Belo Horizonte',
+      jobDescription: ' Futura Estágios Jurídica - Trabalhista amet culpa praesentium necessitatibus facere quae dignissimos sunt.',
+      jobCreatedAt:'13/09 09:25 - Belo Horizonte, MG'
+    },
+    {
+      jobTitle: 'Estagio Cursando Direito - Belo Horizonte',
+      jobDescription: ' Futura Estágios Jurídica - Trabalhista amet culpa praesentium necessitatibus facere quae dignissimos sunt.',
+      jobCreatedAt:'13/09 09:25 - Belo Horizonte, MG'
+    },
+    {
+      jobTitle: 'Estagio Cursando Direito - Belo Horizonte',
+      jobDescription: ' Futura Estágios Jurídica - Trabalhista amet culpa praesentium necessitatibus facere quae dignissimos sunt.',
+      jobCreatedAt:'13/09 09:25 - Belo Horizonte, MG'
+    },
+    {
+      jobTitle: 'Estagio Cursando Direito - Belo Horizonte',
+      jobDescription: ' Futura Estágios Jurídica - Trabalhista amet culpa praesentium necessitatibus facere quae dignissimos sunt.',
+      jobCreatedAt:'13/09 09:25 - Belo Horizonte, MG'
+    },
+    {
+      jobTitle: 'Estagio Cursando Direito - Belo Horizonte',
+      jobDescription: ' Futura Estágios Jurídica - Trabalhista amet culpa praesentium necessitatibus facere quae dignissimos sunt.',
+      jobCreatedAt:'13/09 09:25 - Belo Horizonte, MG'
+    },
+  ]
   constructor(public navCtrl: NavController) {
 
   }
